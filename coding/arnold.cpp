48b9bae1e167d95417f75798493e025b531f4d78
Mulirow <mbn2@cin.ufpe.br>
2021-07-12 20:15:12 -0300

    Added user-defined amount of iterations

--- coding/arnold.cpp
+++ coding/arnold.cpp
@@ -6,33 +6,37 @@
 int main(){
     cv::Mat catImage = cv::imread("../images/input/cat.png", cv::IMREAD_COLOR);
     cv::Mat temp, temp2;
-    int lambdaH, lambdaV;
+    int lambdaH, lambdaV, iterations;
 
     catImage.copyTo(temp);
     catImage.copyTo(temp2);
 
     int height = catImage.rows;
 
-    std::cin >> lambdaH >> lambdaV;
+    std::cin >> lambdaH >> lambdaV >> iterations;
 
-    //Horizontal shearing:
-    //T(x, y) = (x + lambda * y, y) mod height
-    for(int y = 0; y < height; y++){
-        for(int x = 0; x < height; x++){
-            int newX = (x + lambdaH * y) % height;
-            int newY = y % height;
-            temp.at<cv::Vec3b>(newY, newX) = catImage.at<cv::Vec3b>(y, x);
+    while(iterations > 0){
+        //Horizontal shearing:
+        //T(x, y) = (x + lambda * y, y) mod height
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < height; x++){
+                int newX = (x + lambdaH * y) % height;
+                int newY = y % height;
+                temp.at<cv::Vec3b>(newY, newX) = catImage.at<cv::Vec3b>(y, x);
+            }
         }
-    }
 
-    //Vertical shearing:
-    //T(x, y) = (x, lambda * x + y) mod height
-    for(int y = 0; y < height; y++){
-        for(int x = 0; x < height; x++){
-            int newX = x % height;
-            int newY = (lambdaV * x + y) % height;
-            catImage.at<cv::Vec3b>(newY, newX) = temp.at<cv::Vec3b>(y, x);
+        //Vertical shearing:
+        //T(x, y) = (x, lambda * x + y) mod height
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < height; x++){
+                int newX = x % height;
+                int newY = (lambdaV * x + y) % height;
+                catImage.at<cv::Vec3b>(newY, newX) = temp.at<cv::Vec3b>(y, x);
+            }
         }
+        
+        iterations--;
     }
 
     cv::imwrite("../images/output/test.png", catImage);
