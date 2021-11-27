166380d68061ff1304936bbd8a77d167ab0b6842
Mulirow <mbn2@cin.ufpe.br>
2021-11-27 13:26:46 -0300

    Added function to find iterations needed

--- coding/arnold.cpp
+++ coding/arnold.cpp
@@ -5,16 +5,16 @@
 
 void shearing(int, void*);
 
+int findIterations();
+
+cv::Mat source = cv::imread("../images/input/cat-math.png", cv::IMREAD_COLOR);
 cv::Mat catImage = cv::imread("../images/input/cat-math.png", cv::IMREAD_COLOR);
 int tracker = 0, prevTracker = 0;
 int lambdaH = 1, lambdaV = 1;
 
 int main(){
-    cv::Mat source = cv::imread("../images/input/cat-math.png", cv::IMREAD_COLOR);
 
-    int iterations;
-
-    std::cin >> iterations;
+    int iterations = findIterations();
 
     cv::imshow("Arnold's Cat", source);
     cv::createTrackbar("Iterations", "Arnold's Cat", &tracker, iterations, shearing);
@@ -80,4 +80,39 @@ void shearing(int, void*){
 
     cv::imshow("Arnold's Cat", catImage);
     cv::imwrite("../images/output/cat-output.png", catImage);
+}
+
+int findIterations(){
+    cv::Mat temp = catImage.clone();
+    int i = 0;
+    int height = catImage.rows;
+    bool areIdentical = 0;
+
+    while(!areIdentical){
+        //Horizontal shearing:
+        //T(x, y) = (x + lambda * y, y) mod height
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < height; x++){
+                int newX = (x + lambdaH * y) % height;
+                int newY = y % height;
+                temp.at<cv::Vec3b>(newY, newX) = catImage.at<cv::Vec3b>(y, x);
+            }
+        }
+
+        //Vertical shearing:
+        //T(x, y) = (x, lambda * x + y) mod height
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < height; x++){
+                int newX = x % height;
+                int newY = (y + lambdaV * x) % height;
+                catImage.at<cv::Vec3b>(newY, newX) = temp.at<cv::Vec3b>(y, x);
+            }
+        }
+
+        i++;
+
+        areIdentical = !cv::norm(catImage, source);
+    }
+
+    return i;
 }
